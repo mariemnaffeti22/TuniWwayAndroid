c06069ade7853fd3b7e415a475a67f7f2a4f0c16
mariemnaffeti22 <mnaffati67@gmail.com>
2026-05-18 02:42:24 +0100

    feat: UI Login + Register champs visibles

--- app/src/main/java/com/example/tuniwwayandroid/TourAdapter.java
+++ app/src/main/java/com/example/tuniwwayandroid/TourAdapter.java
@@ -7,7 +7,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.bumptech.glide.Glide;
 import java.util.List;
 
 public class TourAdapter extends RecyclerView.Adapter<TourAdapter.TourViewHolder> {
